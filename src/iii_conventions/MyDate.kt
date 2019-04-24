f093c138c6fdea5edbf3c2aa6ecd0915a2e29fcd
Michelle He <mhe@pivotal.io>
2019-04-23 19:54:03 -0700

    finish part iii

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,39 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        when {
+            year > other.year -> return 1
+            year < other.year -> return -1
+            year == other.year -> {
+                when {
+                    month > other.month -> return 1
+                    month < other.month -> return -1
+                    month == other.month -> {
+                        when {
+                            dayOfMonth > other.dayOfMonth -> return 1
+                            dayOfMonth < other.dayOfMonth -> return -1
+                        }
+                    }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+                }
+            }
+        }
+        return 0
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate {
+    return this.addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.int)
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
+    return this.addTimeIntervals(timeInterval, 1)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +41,26 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class RepeatedTimeInterval(val int: Int, val timeInterval: TimeInterval)
+
+operator fun TimeInterval.times(int: Int): RepeatedTimeInterval {
+    return RepeatedTimeInterval(int, this)
+}
+
+class DateRange(
+        override val start: MyDate,
+        override val endInclusive: MyDate
+) : ClosedRange<MyDate>, Iterator<MyDate> {
+
+    private  var current = start
+
+    override fun hasNext(): Boolean {
+        return current <= endInclusive
+    }
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
