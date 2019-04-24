80bffb3c728a44ad9a7e835a084126c257123ef9
Michelle He <mhe@pivotal.io>
2019-04-23 20:08:17 -0700

    refine and shorten the solution

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,39 +1,19 @@
 package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    override fun compareTo(other: MyDate): Int {
-        when {
-            year > other.year -> return 1
-            year < other.year -> return -1
-            year == other.year -> {
-                when {
-                    month > other.month -> return 1
-                    month < other.month -> return -1
-                    month == other.month -> {
-                        when {
-                            dayOfMonth > other.dayOfMonth -> return 1
-                            dayOfMonth < other.dayOfMonth -> return -1
-                        }
-                    }
-
-                }
-            }
-        }
-        return 0
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange {
-    return DateRange(this, other)
-}
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
-operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate {
-    return this.addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.int)
-}
-
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
-    return this.addTimeIntervals(timeInterval, 1)
-}
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval)
+        = this.addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.int)
+operator fun MyDate.plus(timeInterval: TimeInterval)
+        = this.addTimeIntervals(timeInterval, 1)
 
 enum class TimeInterval {
     DAY,
@@ -43,9 +23,7 @@ enum class TimeInterval {
 
 class RepeatedTimeInterval(val int: Int, val timeInterval: TimeInterval)
 
-operator fun TimeInterval.times(int: Int): RepeatedTimeInterval {
-    return RepeatedTimeInterval(int, this)
-}
+operator fun TimeInterval.times(int: Int) = RepeatedTimeInterval(int, this)
 
 class DateRange(
         override val start: MyDate,
